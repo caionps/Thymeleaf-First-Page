1709a1362bb08a96df11e23d5f1fab9614141aaf
caionps <caio.nepos@gmail.com>
2022-07-12 07:13:01 -0300

    first commit

---
+++ thymeleafFirstPage/src/main/java/com/github/caionps/thymeleaffirstpage/ThymeleafFirstPageApplication.java
@@ -0,0 +1,13 @@
+package com.github.caionps.thymeleaffirstpage;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ThymeleafFirstPageApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ThymeleafFirstPageApplication.class, args);
+    }
+
+}
---
+++ thymeleafFirstPage/src/test/java/com/github/caionps/thymeleaffirstpage/ThymeleafFirstPageApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.caionps.thymeleaffirstpage;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ThymeleafFirstPageApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
